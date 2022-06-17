ba198cc5e7c28adf0c44c022293b2aab4085222e
Leonardo Coreixas <lcoreixas@provertec.com.br>
2022-06-17 14:15:59 -0300

    Correção size strings LogEvento

--- Demonstrativo/Models/LogEvento.cs
+++ Demonstrativo/Models/LogEvento.cs
@@ -9,7 +9,11 @@ namespace Demonstrativo.Models
     {
         public int Id { get; set; }
         public int? EventId { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
         public string LogLevel { get; set; }
+
+        [Column(TypeName = "varchar(max)")]
         public string Message { get; set; }
         public DateTime? CreatedTime { get; set; }
     }
